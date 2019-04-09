4bd5e476f7db0a0826f6d823045295d6c14073b1
iamarjun <arjunmanoj1995@gmail.com>
2019-04-09 16:02:25 +0530

    intervla() and timer()

--- app/src/main/java/com/example/reactivex/MainActivity.kt
+++ app/src/main/java/com/example/reactivex/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.reactivex
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -8,12 +9,16 @@ import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //
 
         Observable
             .fromIterable(DataSource.createTasksList())
@@ -28,6 +33,58 @@ class MainActivity : AppCompatActivity() {
 
                 override fun onNext(t: Task) {
                     Log.d(TAG, "onNext: ${t.description}")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+
+
+        /*
+        The Interval operator returns an Observable that emits an infinite sequence of ascending integers,
+        with a constant interval of time of your choosing between emissions.
+        I'm using the takeWhile() operator to perform a check to each emitted value.
+        If the value becomes greater than 5, the observable stops emitting results.
+         */
+
+        Observable
+            .interval(1, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .takeWhile {
+                return@takeWhile it <= 10
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object : Observer<Long> {
+                override fun onComplete() {
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: Long) {
+                    Log.d(TAG, "onNext: $t")
+                }
+
+                override fun onError(e: Throwable) {
+                }
+
+            })
+
+
+        Observable
+            .timer(3, TimeUnit.SECONDS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object: Observer<Long> {
+                override fun onComplete() {
+                }
+
+                override fun onSubscribe(d: Disposable) {
+                }
+
+                override fun onNext(t: Long) {
+                    Log.d(TAG, "timer: $t")
                 }
 
                 override fun onError(e: Throwable) {
